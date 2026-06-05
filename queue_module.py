1a05d1f3a49a628189fe00ce7e33744e8588d4c3
igryhead <131183122+igryhead@users.noreply.github.com>
2026-06-05 20:30:51 +0300

    Update queue_module.py

--- queue_module.py
+++ queue_module.py
@@ -1,41 +1,60 @@
 import ctypes
 
-use_cpp = True
+lib_cpp = ctypes.CDLL("./queue_cpp.dll")
+lib_stl = ctypes.CDLL("./queue_stl.dll") 
 
-if use_cpp:
-    lib = ctypes.CDLL("./queue_cpp.dll")
-else:
-    lib = ctypes.CDLL("./queue.dll")
+lib_cpp.queueInit()
+lib_stl.queueInit()
 
-lib.queueInit()
+_lib = lib_cpp
+
+
+def use_cpp():
+    """Переключиться на реализацию с динамической памятью"""
+    global _lib
+    _lib = lib_cpp
+
+
+def use_stl():
+    """Переключиться на реализацию с STL"""
+    global _lib
+    _lib = lib_stl
+
 
 def enqueue(x):
-    if lib.queueEnqueue(x) == 0:
+    if _lib.queueEnqueue(x) == 0:
         raise Exception("Недостаточно памяти")
+
 
 def dequeue():
     val = ctypes.c_int()
-    if lib.queueDequeue(ctypes.byref(val)) == 0:
+    if _lib.queueDequeue(ctypes.byref(val)) == 0:
         raise Exception("Очередь пуста")
     return val.value
 
+
 def clear():
-    lib.queueClear()
+    _lib.queueClear()
+
 
 def size():
-    return lib.queueSize()
+    return _lib.queueSize()
+
 
 def get_all():
     data = []
     for i in range(size()):
-        data.append(lib.queueGetAt(i))
+        data.append(_lib.queueGetAt(i))
     return data
 
+
 def remove_less(v):
-    return lib.queueRemoveByCondition(0, v)
+    return _lib.queueRemoveByCondition(0, v)
+
 
 def remove_greater(v):
-    return lib.queueRemoveByCondition(1, v)
+    return _lib.queueRemoveByCondition(1, v)
+
 
 def remove_equal(v):
-    return lib.queueRemoveByCondition(2, v)
+    return _lib.queueRemoveByCondition(2, v)
